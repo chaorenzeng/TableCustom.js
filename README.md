54b99a884010b4b82d8ab1c55998f6f9352df514
chaorenzeng <921435247@qq.com>
2019-11-18 17:23:36 +0800

    v2.0.0

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 ### 表格列自定义显示隐藏 (Table column custom display plug-in)
 
 #### 案例展示(Case presentation)
-![查看演示](https://github.com/chaorenzeng/TableCustom.js/blob/master/index.gif)
+![查看演示](https://github.com/chaorenzeng/TableCustom.js/blob/master/case_type1.gif)
+![查看演示](https://github.com/chaorenzeng/TableCustom.js/blob/master/case_type2.gif)
 
 #### 下载地址(Download address)
 > https://github.com/chaorenzeng/TableCustom.js.git
@@ -11,7 +12,7 @@
 1.引入TableCustom.css和TableCustom.js
 ```js
 <link rel="stylesheet" type="text/css" href="js/TableCustom/TableCustom.css" />
-
+...
 <script src="js/TableCustom/TableCustom.js" type="text/javascript" charset="utf-8"></script>
 ```
 2.指定表格Id 配置按钮Id (Specify the table Id configuration button Id)
@@ -20,14 +21,23 @@
 ...
 <button type="button" id="myBtn">TableConfiguration</button>
 ```
-3.创建TableCustom对象 (Create TableCustom objects)
-
+3.创建TableCustom对象 (Create TableCustom objects)  
+- 方式一:默认方式（type1: default mode）
 ```js
 var tabelCustom = new TableCustom({
-	btnId: 'myBtn', //列表配置项按钮Id
-	tableId: 'myTable' //列表配置表格Id
+    btnId: 'myBtn', //列表配置项按钮Id
+    tableId: 'myTable' //列表配置表格Id
 })
 ```
+- 方式二：分类方式（type2: classification mode）
+```js
+var tabelCustom = new TableCustom({
+    btnId: 'myBtn', //列表配置项按钮Id
+    tableId: 'myTable', //列表配置表格Id
+    template: 'byCategory'
+})
+```
+
 #### 支持参数(Support parameters)
 可选参数 Parameter |  默认值 Default | 说明 Introduce
 --        |    --   | --
@@ -37,3 +47,9 @@ title 			|   Table Configuration  | 弹窗标题 (Popup Win title)
 btnAllTxt 		|   all     | 全选按钮文字 (Select all button text)
 btnReverseTxt 	|   reverse | 反选按钮文字 (Uncheck button text)
 btnSaveTxt 		|   save  | 保存按钮文字 (Save button text)
+tipTxt          |         | 弹窗底部说明 (Bottom description)
+template 		|         | 模板 (The template)
+
+#### 如何定制模板 (How to customize templates)
+1. 在tbcustomTemplate中定义模板名和模板信息(Define the template name and template information in tbcustomTemplate)
+2. 编写TableCustom.prototype.getTemplate_<模板名>定制方法 (Write TableCustom. Prototype. GetTemplate_ < template name > custom method)
--- js/TableCustom/TableCustom.js
+++ js/TableCustom/TableCustom.js
@@ -2,7 +2,7 @@
  * TableCustom 表格列自定义显示插件
  * Author: Kevin
  * Introduce: A plug-in to hidden table column 
- * Date: 2019-11-13
+ * Date: 2019-11-18
  */
 
 ;
@@ -15,6 +15,27 @@
 		this._init();
 	}
 
+    //模板
+    var tbcustomTemplate = {
+        byCategory: [
+            {
+                type: 'Category1',
+                sets: ['VEHICLE', 'LICENSE', 'FRAME', 'ENGINE']
+            },
+            {
+                type: 'Category2',
+                sets: ['EQUIPMENT', 'DEVICE', 'INTERNET', 'AFFILIATED']
+            },
+            {
+                type: 'Category3',
+                sets: ['AFFILIATED', 'PHONE']
+            },
+            {
+                type: 'Category4',
+                sets: ['EMAIL', 'ONLINE', 'TIME']
+            }
+        ]
+    }
 	//默认可选参数
 	TableCustom.prototype.defaults = {
 		btnId: '', //配置项按钮
@@ -22,7 +43,9 @@
 		title: 'Table Configuration',
 		btnAllTxt: 'all',
 		btnReverseTxt: 'reverse',
-		btnSaveTxt: 'save'
+		btnSaveTxt: 'save',
+        tipTxt: 'Check the box corresponding to the column you want to keep and click save to generate a custom list',
+        template: ''
 	}
 
 	//初始化
@@ -106,6 +129,7 @@
 						</div>
 						<div class="tbcustom-submit" id="` +
 				tbcustomSaveId + `">`+ this.settings.btnSaveTxt +`</div>
+            <div class="tbcustom-tip">`+ this.settings.tipTxt +`</div>
 					</div>
 				</div>
 			`
@@ -120,212 +144,335 @@
 				self.close();
 			})
 
-			//选项
-			var tbcustomList = this.tbcustomList = document.createElement("ul");
-			tbcustomList.className = "tbcustom-main-list";
-			for (var j = 0, cusLen = theadArray.length; j < cusLen; j++) {
-				var item = theadArray[j],
-					li = document.createElement("li"),
-					input = document.createElement("input"),
-					label = document.createElement("label");
-				input.type = "checkbox";
-				input.checked = item.show;
-				input.id = this.tbcustomId + "-checkbox" + j;
-				input.setAttribute("data-tbcusindex", j);
-				label.innerText = item.title;
-				label.className = "tbcustom-main-checkbox-label"
-				label.setAttribute("for", input.id);
-				li.appendChild(input);
-				li.appendChild(label);
-				tbcustomList.appendChild(li);
-				input.onchange = function(ele) {
-					self.change(ele);
-				}
-			}
-			tbcustom.querySelector(".tbcustom-main").appendChild(tbcustomList)
+            //选项
+            var template = this.getTemplate(theadArray, this.settings.template);
+            tbcustom.querySelector(".tbcustom-main").appendChild(template);
 
-			//全选和反选
-			var tbcustomAllId = this.tbcustomId + "-all",
-				tbcustomReverseId = this.tbcustomId + "-reverse";
-			var tbcustomAll = `
+            //全选和反选
+            var tbcustomAllId = this.tbcustomId + "-all",
+                tbcustomReserveId = this.tbcustomId + "-reserve";
+            var tbcustomAll = `
 				<li>
 					<input type="checkbox" name="" id="` + tbcustomAllId + `"/>
 					<label for="` +
 				tbcustomAllId + `">`+ this.settings.btnAllTxt +`</label>
 				</li>
 				<li>
-					<input type="checkbox" name="" id="` + tbcustomReverseId +
+					<input type="checkbox" name="" id="` + tbcustomReserveId +
 				`"/>
-					<label for="` + tbcustomReverseId +
+					<label for="` + tbcustomReserveId +
 				`">`+ this.settings.btnReverseTxt +`</label>
 				</li>
 				<div class="tbcustom-clearfloat"></div>
 			`;
 			tbcustom.querySelector(".tbcustom-all-items").innerHTML = tbcustomAll;
 			var tbcustomAll = this.tbcustomAll = document.getElementById(tbcustomAllId),
-				tbcustomReverse = this.tbcustomReverse = document.getElementById(tbcustomReverseId);
+				tbcustomReserve = this.tbcustomReserve = document.getElementById(tbcustomReserveId);
 			tbcustomAll.addEventListener("change", function() {
-				tbcustomReverse.checked = false;
+				tbcustomReserve.checked = false;
 				self.all(this.checked)
 			})
-			tbcustomReverse.addEventListener("change", function() {
+			tbcustomReserve.addEventListener("change", function() {
 				tbcustomAll.checked = false;
-				self.reverse(this.checked)
+				self.reserve(this.checked)
 			})
 
-		} else {
-			//已存在时修改
-			this.tbcustomList.querySelectorAll("li").forEach(function(item, i) {
-				item.children[0].checked = theadArray[i].show ? true : false;
-				item.children[1].innerText = theadArray[i].title;
-			})
-		}
-	}
+        } else {
+            //已存在时修改
+            tbcustom.querySelectorAll('input[type ="checkbox"][data-tbcusindex]').forEach(function (item, i) {
+                var thIndex = item.getAttribute('data-tbcusindex');
+                item.checked = theadArray[thIndex].show ? true : false;
+                item.nextSibling.innerText = theadArray[thIndex].title;
+            })
+        }
+    }
 
-	//根据配置项列表，渲染显示表格
-	TableCustom.prototype.render = function(theadArray) {
-		for (var i = 0, tbodyLen = this.table.rows.length; i < tbodyLen; i++) {
-			var row = this.table.rows[i];
-			for (var j = 0, trLen = row.cells.length; j < trLen; j++) {
-				var td = row.cells[j];
-				if (theadArray[j].show) {
-					td.style.display = "";
-				} else {
-					td.style.display = "none";
-				}
-			}
-		}
-	}
 
-	//全选
-	TableCustom.prototype.all = function(type) {
-		for (var i = 0, tempCusLen = this.theadArrayTemp.length; i < tempCusLen; i++) {
-			var item = this.theadArrayTemp[i];
-			item.show = type;
-		}
-		this.toast(this.theadArrayTemp);
-	}
+    //获取配置项显示模板
+    TableCustom.prototype.getTemplate = function (theadArray, templateName) {
+        var self = this,
+            res = getDefaultTpl(), //默认模板
+            template = tbcustomTemplate[templateName],
+            templateRender = this['getTemplate_' + templateName];
 
-	//反选
-	TableCustom.prototype.reverse = function(type) {
-		for (var i = 0, tempCusLen = this.theadArrayTemp.length; i < tempCusLen; i++) {
-			var item = this.theadArrayTemp[i];
-			item.show = !item.show;
-		}
-		this.toast(this.theadArrayTemp)
-	}
+        if (template && templateRender) {
+            //自定义模板
+            res = templateRender(theadArray, template, self);
+        }
 
-	//改变
-	TableCustom.prototype.change = function(ele) {
-		//1.修改theadArrayTemp
-		var item = ele.target,
-			ischecked = item.checked ? true : false,
-			index = item.getAttribute("data-tbcusindex");
-		this.theadArrayTemp[index]["show"] = ischecked;
-		//2.取消反选
-		this.tbcustomReverse.checked = false;
-		//3.判断全选
-		var noAllSelected = this.theadArrayTemp.some(function(obj) {
-			//是否存在未选择选项
-			return !obj.show
-		})
-		if (noAllSelected) {
-			this.tbcustomAll.checked = false;
-		} else {
-			this.tbcustomAll.checked = true;
-		}
-	}
+        //默认模板
+        function getDefaultTpl() {
+            var tbcustomList = document.createElement("ul");
+            tbcustomList.className = "tbcustom-main-list";
+            for (var j = 0, cusLen = theadArray.length; j < cusLen; j++) {
+                //内容
+                var item = theadArray[j],
+                    li = document.createElement("li"),
+                    input = document.createElement("input"),
+                    label = document.createElement("label");
+                input.type = "checkbox";
+                input.checked = item.show;
+                input.id = self.tbcustomId + "-checkbox" + j;
+                input.setAttribute("data-tbcusindex", j);
+                label.innerText = item.title;
+                label.className = "tbcustom-main-checkbox-label"
+                label.setAttribute("for", input.id);
+                li.appendChild(input);
+                li.appendChild(label);
+                tbcustomList.appendChild(li);
+                //事件
+                input.onchange = function (ele) {
+                    self.change(ele);
+                }
+            }
+            return tbcustomList;
+        }
 
-	//保存
-	TableCustom.prototype.save = function() {
-		var theadArray = this.theadArrayTemp;
-		this.theadArray = theadArray.slice(0);
-		this.setTbcustomStorage(theadArray);
-		this.render(theadArray);
-		this.close();
-	}
+        return res;
+    }
 
-	//打开
-	TableCustom.prototype.open = function() {
-		if (this.tbcustom) {
-			this.tbcustom.style.display = "block";
-		}
-	}
+    //根据配置项列表，渲染显示表格
+    TableCustom.prototype.render = function (theadArray) {
+        for (var i = 0, tbodyLen = this.table.rows.length; i < tbodyLen; i++) {
+            var row = this.table.rows[i];
+            for (var j = 0, trLen = row.cells.length; j < trLen; j++) {
+                var td = row.cells[j];
+                if (theadArray[j].show) {
+                    td.style.display = "";
+                } else {
+                    td.style.display = "none";
+                }
+            }
+        }
+    }
 
-	//关闭
-	TableCustom.prototype.close = function(type) {
-		if (this.tbcustom) {
-			//取消全选反选
-			this.tbcustomAll.checked = false;
-			this.tbcustomReverse.checked = false;
-			//更新theadArrayTemp
-			this.theadArrayTemp = objDeepCopy(this.theadArray);
-			this.toast(this.theadArray);
-			//隐藏弹窗
-			this.tbcustom.style.display = "none";
-		}
-	}
+    //全选
+    TableCustom.prototype.all = function (type) {
+        for (var i = 0, tempCusLen = this.theadArrayTemp.length; i < tempCusLen; i++) {
+            var item = this.theadArrayTemp[i];
+            item.show = type;
+        }
+        this.toast(this.theadArrayTemp);
+    }
 
-	//获取缓存
-	TableCustom.prototype.getTbcustomStorage = function() {
-		var storageName = "tbcustom_" + this.settings.tableId;
-		return JSON.parse(localStorage.getItem(storageName) || "[]");
-	}
+    //反选
+    TableCustom.prototype.reserve = function (type) {
+        for (var i = 0, tempCusLen = this.theadArrayTemp.length; i < tempCusLen; i++) {
+            var item = this.theadArrayTemp[i];
+            item.show = !item.show;
+        }
+        this.toast(this.theadArrayTemp)
+    }
 
-	//设置缓存
-	TableCustom.prototype.setTbcustomStorage = function(theadArray) {
-		var storageName = "tbcustom_" + this.settings.tableId;
-		localStorage.setItem(storageName, JSON.stringify(theadArray || []));
-	}
-	
-	//检查缓存是否与表格一致
-	TableCustom.prototype.check = function(thead, theadArrayStorage) {
-		try{
-			if(!theadArrayStorage){
-				return false;
-			}
-			if(!Array.isArray(theadArrayStorage)){
-				return false;
-			}
-			if(theadArrayStorage.length != thead.cells.length){
-				return false;
-			}
-			var isDifferent = theadArrayStorage.some(function(item, index){
-				return item.title != thead.cells[index].innerText;
-			})
-			if(isDifferent){
-				return false;
-			}
-			return true;
-		}catch(e){
-			return false;
-		}
-	}
+    //改变
+    TableCustom.prototype.change = function (ele) {
+        //1.修改theadArrayTemp
+        var item = ele.target,
+            ischecked = item.checked ? true : false,
+            index = item.getAttribute("data-tbcusindex");
+        this.theadArrayTemp[index]["show"] = ischecked;
+        //2.取消反选
+        this.tbcustomReserve.checked = false;
+        //3.判断全选
+        var noAllSelected = this.theadArrayTemp.some(function (obj) {
+            //是否存在未选择选项
+            return !obj.show
+        })
+        if (noAllSelected) {
+            this.tbcustomAll.checked = false;
+        } else {
+            this.tbcustomAll.checked = true;
+        }
+    }
 
-	//参数默认值覆盖
-	function paramsExtend(o, n, override) {
-		for (var key in n) {
-			if (n.hasOwnProperty(key) && (!o.hasOwnProperty(key) || override)) {
-				o[key] = n[key];
-			}
-		}
-		return o;
-	}
+    //保存
+    TableCustom.prototype.save = function () {
+        var theadArray = this.theadArrayTemp;
+        this.theadArray = theadArray.slice(0);
+        this.setTbcustomStorage(theadArray);
+        this.render(theadArray);
+        this.close();
+    }
 
-	//对象数组深拷贝
-	function objDeepCopy(obj) {
-		var result = Array.isArray(obj) ? [] : {};
-		for (var key in obj) {
-			if (obj.hasOwnProperty(key)) {
-				if (typeof obj[key] === 'object') {
-					result[key] = objDeepCopy(obj[key]); // 递归复制
-				} else {
-					result[key] = obj[key];
-				}
-			}
-		}
-		return result;
-	}
+    //打开
+    TableCustom.prototype.open = function () {
+        if (this.tbcustom) {
+            this.tbcustom.style.display = "block";
+        }
+    }
 
-	window.TableCustom = TableCustom;
+    //关闭
+    TableCustom.prototype.close = function (type) {
+        if (this.tbcustom) {
+            //取消全选反选
+            this.tbcustomAll.checked = false;
+            this.tbcustomReserve.checked = false;
+            //更新theadArrayTemp
+            this.theadArrayTemp = objDeepCopy(this.theadArray);
+            this.toast(this.theadArray);
+            //隐藏弹窗
+            this.tbcustom.style.display = "none";
+        }
+    }
+
+    //获取缓存
+    TableCustom.prototype.getTbcustomStorage = function () {
+        var storageName = "tbcustom_" + this.settings.tableId;
+        return JSON.parse(localStorage.getItem(storageName) || "[]");
+    }
+
+    //设置缓存
+    TableCustom.prototype.setTbcustomStorage = function (theadArray) {
+        var storageName = "tbcustom_" + this.settings.tableId;
+        localStorage.setItem(storageName, JSON.stringify(theadArray || []));
+    }
+
+    //检查缓存是否与表格一致
+    TableCustom.prototype.check = function (thead, theadArrayStorage) {
+        try {
+            if (!theadArrayStorage) {
+                return false;
+            }
+            if (!Array.isArray(theadArrayStorage)) {
+                return false;
+            }
+            if (theadArrayStorage.length != thead.cells.length) {
+                return false;
+            }
+            var isDifferent = theadArrayStorage.some(function (item, index) {
+                return item.title != thead.cells[index].innerText;
+            })
+            if (isDifferent) {
+                return false;
+            }
+            return true;
+        } catch (e) {
+            return false;
+        }
+    }
+
+    /*
+    * ====================== 自定义模板格式1 ======================
+	   <table class="tbcustom-main-category">
+		<tbody>
+			<tr>
+				<td class="tbcustom-category-left">基础信息项</td>
+				<td class="tbcustom-category-right">
+					<ul class="tbcustom-category-list">
+						<li>
+							<input type="checkbox" id="tbcustom-mainTable-checkbox0" data-tbcusindex="0">
+							<label class="tbcustom-main-checkbox-label" for="tbcustom-mainTable-checkbox0">ID</label>
+						</li>
+					</ul>
+				</td>
+			</tr>
+		</tbody>
+	   </table>`;
+	*/
+    //获取配置项显示模板：自定义分类1
+    TableCustom.prototype.getTemplate_byCategory = function (theadArray, template, self) {
+        var main = document.createElement("div");
+        main.className = "tbcustom-main-tabel";
+        //携带类别空数组
+        var otherTemplte = {"type": "Others", sets: [], items: []};
+        template = template.map(function (cate, index) {
+            cate.items = [];
+            return cate;
+        })
+        //分类
+        for (var i = 0, thLen = theadArray.length; i < thLen; i++) {
+            var thItem = theadArray[i];
+            thItem['index'] = i; //标识位置
+            for (var j = 0, tpLen = template.length; j < tpLen; j++) {
+                var tpItem = template[j];
+                if (tpItem.sets.indexOf(thItem.title) != -1) {
+                    tpItem.items.push(thItem);
+                    thItem.hadCate = true; //标识已经分类
+                    break;
+                }
+            }
+        }
+        //未分类
+        var otherThead = theadArray.filter(function (item, index) {
+            return !item.hadCate;
+        })
+        otherTemplte.items.push(...otherThead);
+        template.push(otherTemplte);
+
+        var table = document.createElement('table'),
+            tbody = document.createElement('tbody'),
+            checkboxId = 0;
+        table.className = 'tbcustom-main-category';
+        for (var i = 0, tLen = template.length; i < tLen; i++) {
+            //内容
+            var tempItem = template[i];
+            var tr = document.createElement('tr'),
+                list = document.createElement('ul'),
+                trContent =
+                    `
+						<td class="tbcustom-category-left"></td>
+						<td class="tbcustom-category-right"></td>
+					`;
+            list.className = 'tbcustom-category-list';
+            tr.innerHTML = trContent;
+            tr.querySelector('.tbcustom-category-left').innerText = tempItem.type;
+            for (var j = 0, setLen = tempItem.items.length; j < setLen; j++) {
+                var item = tempItem.items[j];
+                var li = document.createElement("li"),
+                    input = document.createElement("input"),
+                    label = document.createElement("label");
+                input.type = "checkbox";
+                input.id = "tbcustom-mainTable-checkbox" + item.index;
+                input.setAttribute('data-tbcusindex', item.index);
+                input.checked = item.show;
+                label.className = "tbcustom-main-checkbox-label";
+                label.setAttribute('for', input.id);
+                label.innerText = item.title;
+                li.appendChild(input);
+                li.appendChild(label);
+                tr.querySelector('.tbcustom-category-left').innerText = tempItem.type;
+                list.appendChild(li);
+                //事件
+                input.onchange = function (ele) {
+                    self.change(ele);
+                }
+                checkboxId++;
+            }
+            tr.querySelector('.tbcustom-category-right').appendChild(list);
+            if (tempItem.items.length > 0) {
+                tbody.appendChild(tr);
+            }
+        }
+        table.appendChild(tbody);
+        main.appendChild(table);
+        return main;
+    }
+
+
+    //参数默认值覆盖
+    function paramsExtend(o, n, override) {
+        for (var key in n) {
+            if (n.hasOwnProperty(key) && (!o.hasOwnProperty(key) || override)) {
+                o[key] = n[key];
+            }
+        }
+        return o;
+    }
+
+    //对象数组深拷贝
+    function objDeepCopy(obj) {
+        var result = Array.isArray(obj) ? [] : {};
+        for (var key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                if (typeof obj[key] === 'object') {
+                    result[key] = objDeepCopy(obj[key]); // 递归复制
+                } else {
+                    result[key] = obj[key];
+                }
+            }
+        }
+        return result;
+    }
+
+    window.TableCustom = TableCustom;
 })(window, document);
